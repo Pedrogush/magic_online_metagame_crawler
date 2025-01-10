560dd24845dac255057b12dcc497c431dd2eb25c
Pedrogush <pedrogush@gmail.com>
2025-01-10 10:58:48 -0300

    netdecker get daily decks from goldfish

--- netdecker.py
+++ netdecker.py
@@ -14,7 +14,46 @@ def mtggoldfish_get_archetypes(mtg_format: str):
     return set(tag.text.strip() for tag in archetypes)
 
 
-def mtggoldfish_get_deck(self, deck_num: str):
+def mtggoldfish_get_daily_decks(mtg_format: str):
+    page = requests.get(f'https://www.mtggoldfish.com/metagame/{mtg_format}')
+    soup = bs4.BeautifulSoup(page.text, 'html.parser')
+    table_container = soup.select_one("div.similar-events-container")
+    h4s: list[bs4.Tag] = table_container.find_all('h4')
+    decks = {}
+    for h4 in h4s:
+        date = h4.find('nobr').text.strip().replace('on ', '')
+        tournament_type = h4.find('a').text.strip()
+        has_placement = 'challenge' in tournament_type.lower()
+        if date not in decks:
+            decks[date] = []
+        tbody: bs4.Tag = h4.find_next_sibling()
+        cells = tbody.select("tr.striped")
+        for cell in cells:
+            deck_name = cell.select_one('td.column-deck').select_one('span.deck-price-paper').text.strip()
+            deck_number = (
+                cell
+                .select_one('td.column-deck')
+                .select_one('a')['href']
+                .replace('#online', '')
+                .replace('/deck/', '')
+            )
+            player_name = cell.select_one('td.column-player').text.strip()
+            placement = None
+            if has_placement:
+                placement = cell.select_one('td.column-place').text.strip()
+            decks[date].append(
+                {
+                    'deck_name': deck_name,
+                    'player_name': player_name,
+                    'tournament_type': tournament_type,
+                    'deck_number': deck_number,
+                    'placement': placement
+                }
+            )
+    return decks
+
+
+def mtggoldfish_download_deck(self, deck_num: str):
     file = requests.get(f'https://www.mtggoldfish.com/deck/download/{deck_num}')
     with open('curr_deck.txt', 'wb') as f:
         f.write(file.content)
@@ -41,4 +80,4 @@ class Netdecker:
 if __name__ == '__main__':
     # nd = Netdecker()
     # nd.driver.maximize_window()
-    print(mtggoldfish_get_archetypes('modern'))
+    print(mtggoldfish_get_daily_decks('modern'))
