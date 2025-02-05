dfc4ae46d6f36b7dd9119fab0169782cd774b4f2
Pedrogush <pedrogush@gmail.com>
2025-02-04 22:44:13 -0300

    change to grid for best practices, increment/decrement/remove cards working ok

--- widgets/deck_selector.py
+++ widgets/deck_selector.py
@@ -1,5 +1,6 @@
 import time
 import tkinter as tk
+from tkinter import ttk
 import json
 import pyperclip
 from navigators.mtgo import login
@@ -21,13 +22,6 @@ from navigators.mtgo import (
     register_deck,
 )
 
-# TODO: add buttons
-# return to select archetype state
-# clear buffer
-# clear list
-# round decklist down
-# round decklist up
-# round decklist
 FORMAT_OPTIONS = [
     "Modern",
     "Standard",
@@ -86,13 +80,12 @@ def frame(root, name, color=CS[3]):
             foreground="black",
             relief="solid",
         )
-        frame_title.pack(anchor="center", expand=False, fill="both")
+        frame_title.grid(column=0, row=0, sticky="nsew")
     return frame
 
 
 def b_frame(root, color=CS[3]):
     frame = tk.Frame(root, relief="solid", padx=3, pady=0, background=color, borderwidth=0, height=15)
-    frame.pack(anchor="center", fill="y", side=tk.TOP, expand=True)
     return frame
 
 
@@ -153,39 +146,62 @@ class MTGDeckSelectionWidget:
         self.listbox_button.unbind("<<ListboxSelect>>")
         self.listbox_button.config(text="Select archetype", command=self.select_archetype)
         repopulate_listbox(self.listbox, [archetype["name"] for archetype in self.archetypes])
-        self.reset_button.forget()
+        if hasattr(self, "reset_button"):
+            self.reset_button.forget()
 
     def ui_make_components(self):
         self.root.title("MTG Helper")
-        self.ui_create_frames()
-        self.ui_create_buttons()
-        self.ui_create_listbox()
+        self.ui_create()
         self.ui_populate_archetypes()
-        self.ui_create_textbox()
-        self.ui_pack_components()
         self.ui_bind_components()
 
-    def ui_create_frames(self):
+    def ui_create(self):
         self.F_top = frame(self.root, "", color="bisque4")
+        self.F_top.grid(column=0, row=0, sticky="nsew")
         self.F_top_left = frame(self.F_top, "Manatraders Card Rental Automation", color=CS[1])
+        self.F_top_left.rowconfigure(2, weight=1)
+        self.F_top_left.grid(column=0, row=1, sticky="nsew")
         self.F_top_right = frame(self.F_top, "Decklist", color=CS[1])
+        self.F_top_right.grid(column=1, row=1, sticky="nsew")
         self.F_top_right_top = frame(self.F_top_right, "", color=CS[2])
+        self.F_top_right_top.grid(column=1, row=0, sticky="nsew")
         self.F_top_textbox = frame(self.F_top_right, "", color=CS[2])
+        self.F_top_textbox.grid(column=0, row=0, sticky="nsew")
+        self.F_top_textbox.rowconfigure(0, weight=1)
         self.F_bottom = frame(self.root, "Configuration", color=CS[3])
+        self.F_bottom.grid(column=0, row=2, sticky="nsew")
 
-    def ui_create_buttons(self):
-        self.save_deck_button: tk.Button
         self.save_deck_button = button(self.F_top_right_top, "Save deck", self.save_deck_as)
+        self.save_deck_button.grid(column=0, row=0, sticky="nsew")
         self.add_deck_to_buffer_button = button(self.F_top_right_top, "Add deck to buffer", self.add_deck_to_buffer)
+        self.add_deck_to_buffer_button.grid(column=1, row=0, sticky="nsew")
         self.make_average_deck_button = button(self.F_top_right_top, "Mean of buffer", self.make_average_deck)
-        self.make_daily_average_deck_button = button(self.F_top_right_top, "Day's Average", self.set_daily_average_deck)
-        self.reset_button = button(self.F_top_left, "Reset", self.ui_reset_to_archetype_selection)
+        self.make_average_deck_button.grid(column=2, row=0, sticky="nsew")
         self.login_button = button(self.F_bottom, "MTGO Login", login)
+        self.login_button.grid(column=0, row=0, sticky="nsew")
         self.listbox_button = button(self.F_top_left, "Select archetype", self.select_archetype)
+        self.listbox_button.grid(column=0, row=0, sticky="nsew")
         self.return_cards_button = button(self.F_top_left, "Return cards", self.return_cards)
-        self.ui_format_menu()
-
-    def ui_format_menu(self):
+        self.return_cards_button.grid(column=0, row=1, sticky="nsew")
+        self.listbox = tk.Listbox(
+            self.F_top_left,
+            selectmode=tk.SINGLE,
+            background=CS[4],
+            foreground="black",
+            font=("calibri", 15, "bold"),
+        )
+        self.listbox.grid(column=0, row=2, sticky="nsew")
+        self.listbox_scrollbar = tk.Scrollbar(self.F_top_left, orient="vertical")
+        self.listbox_scrollbar.grid(column=1, row=2, sticky="nsew")
+        self.listbox.config(yscrollcommand=self.listbox_scrollbar.set)
+        self.listbox_scrollbar.config(command=self.listbox.yview)
+        self.textbox = tk.Text(
+            self.F_top_textbox,
+            font=("calibri", 15, "bold"),
+            background=CS[1],
+            foreground="black",
+        )
+        self.textbox.grid(column=0, row=0, sticky="nsew")
         self.choose_format_button = tk.OptionMenu(
             self.F_bottom,
             self.format,
@@ -193,6 +209,7 @@ class MTGDeckSelectionWidget:
             *FORMAT_OPTIONS[1:],
             command=lambda x: self.choose_format_button_clicked(),
         )
+        self.choose_format_button.grid(column=0, row=0, sticky="nsew")
         choose_format_button_config(self.choose_format_button)
 
     def choose_format_button_clicked(self):
@@ -206,30 +223,13 @@ class MTGDeckSelectionWidget:
         for index, archetype in enumerate(self.archetypes):
             self.listbox.insert(index, archetype["name"])
 
-    def ui_create_listbox(self):
-        self.listbox = tk.Listbox(
-            self.F_top_left,
-            selectmode=tk.SINGLE,
-            background=CS[4],
-            foreground="black",
-            font=("calibri", 15, "bold"),
-        )
-        self.listbox_scrollbar = tk.Scrollbar(self.listbox, orient="vertical")
-
-    def ui_create_textbox(self):
-        self.textbox = tk.Text(
-            self.F_top_textbox,
-            font=("calibri", 15, "bold"),
-            background=CS[1],
-            foreground="black",
-        )
-        self.textbox_plus_buttons: list[tk.Button] = []
-        self.textbox_minus_buttons: list[tk.Button] = []
-        self.textbox_remove_buttons: list[tk.Button] = []
-
     def save_deck_as(self):
         logger.debug(self.currently_selected_deck)
-        deck_name = f"{format_deck_name(self.currently_selected_deck)}.txt"
+        if not self.currently_selected_deck:
+            date = time.strftime("%Y-%m-%d-%H-%M-%S")
+            deck_name = f"edited_deck_{date}.txt"
+        else:
+            deck_name = f"{format_deck_name(self.currently_selected_deck)}.txt"
         with open(CONFIG["deck_selector_save_path"] + deck_name, "w") as f:
             f.write(self.textbox.get("1.0", tk.END))
 
@@ -264,24 +264,6 @@ class MTGDeckSelectionWidget:
     def ui_bind_components(self):
         self.textbox.bind("<Key>", self.textbox_on_change)
 
-    def ui_pack_components(self):
-        self.login_button.pack(anchor="center", fill="x", side=tk.LEFT, expand=False)
-        self.choose_format_button.pack(anchor="center", fill="both", side=tk.RIGHT, expand=False)
-        self.listbox.pack(anchor="center", fill="both", side=tk.BOTTOM, expand=True)
-        self.listbox_button.pack(anchor="center", fill="both", side=tk.BOTTOM, expand=False)
-        self.listbox_scrollbar.pack(anchor="center", fill="y", side=tk.RIGHT)
-        self.return_cards_button.pack(anchor="center", fill="both", side=tk.BOTTOM, expand=False)
-        self.save_deck_button.pack(anchor="center", fill="both", side=tk.LEFT, expand=False)
-        self.add_deck_to_buffer_button.pack(anchor="center", fill="both", side=tk.LEFT, expand=False)
-        self.make_average_deck_button.pack(anchor="center", fill="both", side=tk.LEFT, expand=False)
-        self.textbox.pack(anchor="center", fill="both", side=tk.LEFT, expand=True)
-        self.F_top.pack(anchor="center", fill="both", side=tk.TOP, expand=True)
-        self.F_top_right.pack(anchor="center", fill="both", side=tk.RIGHT, expand=True)
-        self.F_top_right_top.pack(anchor="center", fill="both", side=tk.TOP, expand=False)
-        self.F_top_left.pack(anchor="center", fill="both", side=tk.LEFT, expand=True)
-        self.F_bottom.pack(anchor="center", fill="x", side=tk.BOTTOM, expand=False)
-        self.F_top_textbox.pack(anchor="center", fill="both", side=tk.RIGHT, expand=False)
-
     def select_archetype(self):
         selected = self.listbox.curselection()
         if not selected:
@@ -292,8 +274,10 @@ class MTGDeckSelectionWidget:
         repopulate_listbox(self.listbox, [format_deck_name(deck) for deck in self.decks])
         self.listbox_button.config(text="Select deck", command=self.select_deck)
         self.listbox.bind("<<ListboxSelect>>", self.set_textbox)
-        self.reset_button.pack(anchor="center", fill="both", side=tk.BOTTOM, expand=False)
-        self.make_daily_average_deck_button.pack(anchor="center", fill="both", side=tk.BOTTOM, expand=False)
+        self.reset_button = button(self.F_top_left, "Reset", self.ui_reset_to_archetype_selection)
+        self.reset_button.grid(column=0, row=3, sticky="nsew")
+        self.make_daily_average_deck_button = button(self.F_top_right_top, "Day's Average", self.set_daily_average_deck)
+        self.make_daily_average_deck_button.grid(column=3, row=0, sticky="nsew")
 
     def set_daily_average_deck(self):
         today = time.strftime("%Y-%m-%d")
@@ -317,71 +301,95 @@ class MTGDeckSelectionWidget:
         self.textbox.delete("1.0", tk.END)
         download_deck(deck["number"])
         self.textbox.insert("1.0", open("curr_deck.txt").read())
-        lines = self.textbox.get("1.0", tk.END).split("\n")        
+        lines = self.textbox.get("1.0", tk.END).split("\n")
         self.set_textbox_buttons(lines)
 
     def set_textbox_buttons(self, lines):
-        accum_padding = 0
         self.q_btn_frames = []
-        for line in lines:
+        sideboard_index = lines.index("")
+        is_sideboard = False
+        self.textbox.delete("1.0", tk.END)
+        for i, line in enumerate(lines):
             FONT = ("verdana", 7)
             if not line.strip():
                 empty_frame = b_frame(self.F_top_textbox, color=CS[2])
-                empty_frame.pack(anchor="center", fill="both", side=tk.TOP, expand=False)
+                self.textbox.window_create(tk.END, window=empty_frame)
                 self.q_btn_frames.append(empty_frame)
                 continue
-            qtn_btns_frame = b_frame(self.F_top_textbox, color=CS[2])
-            self.q_btn_frames.append(qtn_btns_frame)
-            plus_btn = b_button(qtn_btns_frame, "+", lambda line=line: self.increment_card(line), font=FONT)
-            minus_btn = b_button(qtn_btns_frame, "-", lambda line=line: self.decrement_card(line), font=FONT)
-            remove_btn = b_button(qtn_btns_frame, "X", lambda line=line: self.remove_card(line), font=FONT)
-            plus_btn.pack(anchor="center", fill="x", side=tk.LEFT, expand=False)
-            minus_btn.pack(anchor="center", fill="x", side=tk.LEFT, expand=False)
-            remove_btn.pack(anchor="center", fill="x", side=tk.LEFT, expand=False)
-            if accum_padding < 8:
-                qtn_btns_frame.pack(anchor="center", fill="both", side=tk.TOP, expand=False, ipady=2)
-                accum_padding += 2
-                continue
-            qtn_btns_frame.pack(anchor="center", fill="both", side=tk.TOP, expand=False, ipady=0)
-            accum_padding = 0
+            if i > sideboard_index:
+                is_sideboard = True
+            F_edit_deck = self.create_F_edit_deck(line, is_sideboard)
+            self.q_btn_frames.append(F_edit_deck)
+            self.textbox.insert(tk.END, line)
+            self.textbox.window_create(tk.END, window=F_edit_deck)
+            self.textbox.insert(tk.END, "\n")
 
-    def increment_card(self, line):
-        card = ' '.join(line.split(" ")[1:])
+    def set_textbox_scrollbar(self):
+        self.textbox_scrollbar = tk.Scrollbar(self.F_top_textbox, orient="vertical")
+        self.textbox_scrollbar.config(command=self.textbox.yview)
+
+    def increment_card(self, line, is_sideboard=False):
+        card = " ".join(line.split(" ")[1:])
         lines = self.textbox.get("1.0", tk.END).split("\n")
+        logger.debug(lines)
+        num_occurrences = len([card_description for card_description in lines if card in card_description])
+        if num_occurrences == 0:
+            logger.debug(f"{card} not found in decklist")
+            return
         index = [lines.index(card_description) for card_description in lines if card in card_description][0]
-        for curr_line in lines:
-            if card not in curr_line:
-                continue
-            amount = int(float(curr_line.split(" ")[0]))
-            amount += 1
-            line = f"{str(amount)} {''.join(card)}"
+        if num_occurrences == 2:
+            index = [lines.index(lin) for lin in lines if card in lin][is_sideboard]
+        curr_line = lines[index]
+        amount = int(float(curr_line.split(" ")[0]))
+        amount += 1
+        line = f"{str(amount)} {''.join(card)}"
         lines[index] = line
-        self.textbox.delete("1.0", tk.END)
-        self.textbox.insert("1.0", "\n".join(lines))
+        self.textbox.replace(f"{index + 1}.0", f"{index + 1}.end", line)
+        F_edit_deck = self.create_F_edit_deck(line, is_sideboard)
+        self.q_btn_frames.append(F_edit_deck)
+        self.textbox.window_create(f"{index + 1}.end", window=F_edit_deck)
 
-    def decrement_card(self, line):
-        card = ' '.join(line.split(" ")[1:])
+    def decrement_card(self, line, is_sideboard=False):
+        card = " ".join(line.split(" ")[1:])
         lines = self.textbox.get("1.0", tk.END).split("\n")
+        logger.debug(lines)
+        num_occurrences = len([card_description for card_description in lines if card in card_description])
+        if num_occurrences == 0:
+            logger.debug(f"{card} not found in decklist")
+            return
         index = [lines.index(card_description) for card_description in lines if card in card_description][0]
-        for curr_line in lines:
-            if card not in curr_line:
-                continue
-            amount = int(float(curr_line.split(" ")[0]))
-            amount -= 1
-            line = f"{str(amount)} {''.join(card)}"
+        if num_occurrences == 2:
+            index = [lines.index(lin) for lin in lines if card in lin][is_sideboard]
+        curr_line = lines[index]
+        amount = int(float(curr_line.split(" ")[0]))
+        amount -= 1
+        line = f"{str(amount)} {''.join(card)}"
         lines[index] = line
-        self.textbox.delete("1.0", tk.END)
-        self.textbox.insert("1.0", "\n".join(lines))
+        self.textbox.replace(f"{index + 1}.0", f"{index + 1}.end", line)
+        F_edit_deck = self.create_F_edit_deck(line, is_sideboard)
+        self.q_btn_frames.append(F_edit_deck)
+        self.textbox.window_create(f"{index + 1}.end", window=F_edit_deck)
 
-    def remove_card(self, line):
-        card = ' '.join(line.split(" ")[1:])
+    def create_F_edit_deck(self, line, is_sideboard):
+        F_edit_deck = b_frame(self.F_top_textbox, color=CS[2])
+        self.q_btn_frames.append(F_edit_deck)
+        FONT = ("verdana", 7)
+        plus_btn = b_button(F_edit_deck, "+", lambda line=line: self.increment_card(line, is_sideboard), font=FONT)
+        minus_btn = b_button(F_edit_deck, "-", lambda line=line: self.decrement_card(line, is_sideboard), font=FONT)
+        remove_btn = b_button(F_edit_deck, "X", lambda line=line: self.remove_card(line, is_sideboard), font=FONT)
+        plus_btn.grid(column=1, row=0)
+        minus_btn.grid(column=2, row=0)
+        remove_btn.grid(column=3, row=0)
+        return F_edit_deck
+
+    def remove_card(self, line, is_sideboard):
+        card = " ".join(line.split(" ")[1:])
         lines = self.textbox.get("1.0", tk.END).split("\n")
+        num_occurrences = len([card_description for card_description in lines if card in card_description])
         index = [lines.index(card_description) for card_description in lines if card in card_description][0]
-        lines.pop(index)
-        self.textbox.delete("1.0", tk.END)
-        self.textbox.insert("1.0", "\n".join(lines))
-        self.q_btn_frames[index].pack_forget()
-        self.q_btn_frames.pop(index)
+        if num_occurrences == 2:
+            index = [lines.index(lin) for lin in lines if card in lin][is_sideboard]
+        self.textbox.replace(f"{index + 1}.0", f"{index + 2}.0", '')
 
     def select_deck(self):
         selected = self.listbox.curselection()
@@ -427,12 +435,12 @@ class MTGDeckSelectionWidget:
         webdriver.driver.quit()
 
     def hide_labels(self):
-        self.choose_format_button.pack_forget()
-        self.login_button.pack_forget()
+        self.choose_format_button.forget()
+        self.login_button.forget()
 
     def show_labels(self):
-        self.choose_format_button.pack(anchor="center", fill="both", side=tk.RIGHT, expand=True)
-        self.login_button.pack(anchor="center", fill="both", side=tk.BOTTOM, expand=True)
+        self.choose_format_button.grid(column=1, row=0, sticky="nsew")
+        self.login_button.grid(column=0, row=0, sticky="nsew")
 
     def save_config(self):
         config = {
